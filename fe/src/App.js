c6bbbbd8a15d94ca954417cb53cf73a337d9b43d
RyanRamadhianto <86404155+RyanRamadhianto@users.noreply.github.com>
2021-10-04 12:36:33 +0700

    1

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 
@@ -8,5 +9,18 @@ function App() {
     </>
   );
 }
+=======
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import Profile from './pages/Profile';
 
-export default App;
+const App = () => {
+  return <Profile/>;
+};
+>>>>>>> Stashed changes
+
+export default App
