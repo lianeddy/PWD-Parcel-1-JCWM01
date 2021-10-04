241fa81f93a99e0ebe4cb8d4b8206f26676bf1ce
Ivanstwan <ivan.stwan@gmail.com>
2021-10-04 11:53:59 +0700

    [PWD-16] Update

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
     <>
-      <Router>
-        <RegisterPage />
-      </Router>
+      <Router>{/* <RegisterPage /> */}</Router>
     </>
   );
 }
