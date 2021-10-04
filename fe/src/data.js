1cfc9a1534db9ceaeb4a88bb53854109a48bba7b
galihwisnu <81431152+galihwisnu@users.noreply.github.com>
2021-10-04 19:32:33 +0700

    [PWD-10]-Login

---
+++ fe/src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://asset.indosport.com/article/image/q/80/171664/1-169.jpg?w=750&h=423",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://i.ibb.co/DG69bQ4/2.png",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://i.ibb.co/cXFnLLV/3.png",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "SHIRT STYLE!",
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "LOUNGEWEAR LOVE",
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      title: "LIGHT JACKETS",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5S_r4fNzvg60bdbDGqW3Rc-SoeP6ns5AMxX-SEAYzLm6oFbdUz5fLvHTznD6L5J2wz2U&usqp=CAU",
+    },
+    {
+      id:2,
+      img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxuyceadRWJsDARbjLYr_zaiJ0SV_Aj5X_TYXWzCtuzIsug8SXrmycVqUYd0W9sER_M1o&usqp=CAU",
+    },
+    {
+      id:3,
+      img:"https://previews.123rf.com/images/jointstar/jointstar1411/jointstar141100017/33364150-kitkat-isolate-on-white-background.jpg",
+    },
+    {
+      id:4,
+      img:"https://www.designerpeople.com/wp-content/uploads/2021/04/chips-packaging-design-1.jpg",
+    },
+    {
+      id:5,
+      img:"https://www.designerpeople.com/wp-content/uploads/2021/04/product-photo-shoot-1.jpg",
+    },
+    {
+      id:6,
+      img:"https://m.media-amazon.com/images/I/81lSD-6llpS._SL1500_.jpg",
+    },
+    {
+      id:7,
+      img:"https://fast.clickbooq.com/4a9d64-986/130308_ProductTest_1888_FF-lg.jpg",
+    },
+    {
+      id:8,
+      img:"https://previews.123rf.com/images/denismart/denismart1809/denismart180900198/111248991-london-uk-august-10-2018-aluminium-can-of-lipton-ice-tea-with-lemon-flavour-on-white-.jpg",
+    },
+  ]
