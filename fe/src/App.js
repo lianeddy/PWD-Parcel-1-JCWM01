0de7ea5f84ea0ac257e1bd442762805489f01506
RyanRamadhianto <86404155+RyanRamadhianto@users.noreply.github.com>
2021-10-04 13:55:23 +0700

    Update App.js

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,26 +1,14 @@
-<<<<<<< Updated upstream
-import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-
-function App() {
-  return (
-    <>
-      <Router>{/* <RegisterPage /> */}</Router>
-    </>
-  );
-}
-=======
 import Product from "./pages/Product";
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
-import Profile from './pages/Profile';
+import Profile from "./pages/Profile";
+
 
 const App = () => {
-  return <Profile/>;
+  return <Pro/>;
 };
->>>>>>> Stashed changes
 
-export default App
+export default App;``
