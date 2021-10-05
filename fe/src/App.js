c1407f7503778946c7333035ca9863512cfe4845
Ivanstwan <ivan.stwan@gmail.com>
2021-10-05 09:34:50 +0700

    [PWD-16] Ubah UI

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
     <>
-      <Router>{/* <RegisterPage /> */}</Router>
+      <Router>
+        <RegisterPage />
+      </Router>
     </>
   );
 }
