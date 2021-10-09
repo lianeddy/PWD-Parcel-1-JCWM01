76e9a1a8c2798cc0e062ad97714e762dfcd0218f
RyanRamadhianto <86404155+RyanRamadhianto@users.noreply.github.com>
2021-10-09 13:27:13 +0700

    12

--- fe/src/App.js
+++ fe/src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 // import RegisterPage from "./pages/RegisterPage";
 import EditProfile from "./components/EditProfile";
-import UserProfile from "./components/UserProfile";
+import userProfile from "./components/userProfile";
 // commect
 
 function App() {
   return (
     <>
       <Router>
-        <Route path="/profile" component={UserProfile} />
+        <Route path="/profile" component={userProfile} />
         <Route path="/edit" component={EditProfile} />
       </Router>
     </>
