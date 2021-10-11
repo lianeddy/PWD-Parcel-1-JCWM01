c152a5c0c4f19dba5fe798533e51a6a98d323cc9
RyanRamadhianto <86404155+RyanRamadhianto@users.noreply.github.com>
2021-10-12 00:21:58 +0700

    12

--- fe/src/components/UserTransaksi.js
+++ fe/src/components/UserTransaksi.js
@@ -1,38 +1,26 @@
 import React, { useState,useEffect } from "react";
 import Axios from "axios";
 import { Link } from "react-router-dom";
-import "./UserProfile.css";
+import "./userTransaksi.css";
 
 import {Button} from "react-bootstrap";
 
 
 function UserTransaksi() {
+  const [transactions, setUserTransaksi] = useState([]);
   const [userData] = useState({});
-  const [no_order, setNoOrder] = useState();
-  const [status, setStatus] = useState();
-  const [created_at, setDate] = useState();
-  const [total, setTotal] = useState();
-  const [parcel_id, setParcel] = useState();
-  const [quantity, setQuantity] = useState();
-  const [harga, setHarga] = useState();
   
   let id = 1
-  const user = () => {
+  const user = async () => {
     console.log(userData);
 
     // ngambil data dari database by id yang login
     Axios.get(`http://localhost:3302/order?id=${id}`, userData)
-      .then((res) => {
+      .then(res => {
+        setUserTransaksi(res.data)
         console.log(res.data);
-        setNoOrder(res.data[0].no_order);
-        setStatus(res.data[0].status);
-        setDate(res.data[0].created_at);
-        setTotal(res.data[0].total);
-        setParcel(res.data[0].parcel_id);
-        setQuantity(res.data[0].quantity);
-        setHarga(res.data[0].harga);
-
-        // console.log(userEdit);
+        console.log(transactions)
+  
       })
       .catch((err) => console.log(err));
   };
@@ -41,7 +29,7 @@ function UserTransaksi() {
   useEffect(() => {
     console.log("test")
     user()
-  });
+  }, []);
 
   // FOR ADMIN CHECKING USER
   // const get = () => {
@@ -54,52 +42,65 @@ function UserTransaksi() {
 
   return (
     <>
-      <div className="wrapper">
-        <div className="reg-container">
-          <div className="UserProfile-side">
-            <div className="reg-icon">
+    <div className="container">
+      <div className="row">
+    {transactions.map(transaction => (
+      <div className="card">
+        <div className="card-body">
+          <div className="row">
+            <div className="col">
+              <label>
+                <h2 className="status">Status</h2>
+                <p>{transaction.status}</p>
+              </label>
             </div>
-            <div className="right">
-              <h1 className="info">Your Transactions</h1>
+            <div className="col">
+              <label>
+                <h2 className="date">Date</h2>
+                <p>{transaction.created_at}</p>
+              </label>
             </div>
-            <div className="right">
-              <form className="right-form">
-                <label>
-                  <h2 className="status">Status</h2>
-                  <p>{status}</p>
-                </label>
-                <label>
-                  <h2 className="date">Date</h2>
-                  <p>{created_at}</p>
-                </label>
-                <label>
-                  <h2 className="parcel">Order Number</h2>
-                  <p>{no_order}</p>
-                </label>
-
-                <label>
-                  <h2 className="data">Quantity</h2>
-                  <p>{quantity}</p>
-                </label>
-                <label>
-                  <h2 className="data">Parcel Price</h2>
-                  <p>{harga}</p>
-                </label>
-                <label>
-                  <h2 className="data">Total</h2>
-                  <p>{total}</p>
-                </label>
-                <Link to={`/edit/${id}`}>
-                  <Button className="reg-button" type="button">
-                  Order Detail
-                </Button>
-                 </Link>
-              </form>
+            <div className="col">
+              <label>
+                <h2 className="parcel">Order Number</h2>
+                <p>{transaction.no_order}</p>
+              </label>
             </div>
           </div>
-          <div className="content-side"></div>
+          <div className="row">
+            <div className="col">
+              <label>
+                <h2 className="data">Parcel</h2>
+                <p>{transaction.nama}</p>
+              </label>
+            </div>
+            <div className="col">
+              <label>
+                <h2 className="data">Quantity</h2>
+                <p>{transaction.quantity}</p>
+              </label>
+            </div>
+            <div className="col">
+              <label>
+                <h2 className="data">Parcel Price</h2>
+                <p>{transaction.harga}</p>
+              </label>
+            </div>
+            <div className="col">
+              <label>
+                <h2 className="data">Total</h2>
+                <p>{transaction.total}</p>
+              </label>
+            </div>
+          </div>
         </div>
+        <div className="content-side"></div>
       </div>
+      
+    ))}
+    </div>
+    </div>
+      
     </>
   );
 }
