1ae8339cbcfa816848a85c958d5e6bc1fef9beb6
RyanRamadhianto <86404155+RyanRamadhianto@users.noreply.github.com>
2021-10-11 15:39:23 +0700

    12

--- fe/src/components/UserTransaksi.js
+++ fe/src/components/UserTransaksi.js
@@ -1,202 +1,107 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import Axios from 'axios'
-import { API_URL } from "../constans/API"
-import { getCartData } from '../redux/actions/cart';
+import React, { useState,useEffect } from "react";
+import Axios from "axios";
+import { Link } from "react-router-dom";
+import "./UserProfile.css";
 
-class Cart extends React.Component {
-  state = {
-    isCheckoutMode: false,
-    recipientName: "",
-    address: "",
-    payment: 0,
-  }
+import {Button} from "react-bootstrap";
 
-  deleteCartHandler = (cartId) => {
-    Axios.delete(`${API_URL}/carts/${cartId}`)
-    .then(() => {
-      this.props.getCartData(this.props.userGlobal.id)
-    })
-    .catch(() => {
-      alert("Terjadi kesalahan di server")
-    })
-  }
 
-  renderCart = () => {
-    return this.props.cartGlobal.cartList.map((val) => {
-      return (
-        <tr>
-          <td className="align-middle">
-            {val.productName}
-          </td>
-          <td className="align-middle">
-            {val.price}
-          </td>
-          <td className="align-middle">
-            <img src={val.productImage} alt="" style={{ height: "125px" }} />
-          </td>
-          <td className="align-middle">
-            {val.quantity}
-          </td>
-          <td className="align-middle">
-            {val.quantity * val.price}
-          </td>
-          <td className="align-middle">
-            <button onClick={() => this.deleteCartHandler(val.id)} className="btn btn-danger">
-              Delete
-            </button>
-          </td>
-        </tr>
-      )
-    })
-  }
+function UserTransaksi() {
+  const [userData] = useState({});
+  const [no_order, setNoOrder] = useState();
+  const [status, setStatus] = useState();
+  const [created_at, setDate] = useState();
+  const [total, setTotal] = useState();
+  const [parcel_id, setParcel] = useState();
+  const [quantity, setQuantity] = useState();
+  const [harga, setHarga] = useState();
+  
+  let id = 1
+  const user = () => {
+    console.log(userData);
 
-  renderSubtotalPrice = () => {
-    let subtotal = 0;
-    for(let i = 0; i < this.props.cartGlobal.cartList.length; i++) {
-      subtotal += this.props.cartGlobal.cartList[i].price * this.props.cartGlobal.cartList[i].quantity
-    }
+    // ngambil data dari database by id yang login
+    Axios.get(`http://localhost:3302/order?id=${id}`, userData)
+      .then((res) => {
+        console.log(res.data);
+        setNoOrder(res.data[0].no_order);
+        setStatus(res.data[0].status);
+        setDate(res.data[0].created_at);
+        setTotal(res.data[0].total);
+        setParcel(res.data[0].parcel_id);
+        setQuantity(res.data[0].quantity);
+        setHarga(res.data[0].harga);
 
-    return subtotal;
-  }
-
-  renderTaxFee = () => {
-    return this.renderSubtotalPrice() * 0.05;
-  }
-
-  renderTotalPrice = () => {
-    return this.renderSubtotalPrice() + this.renderTaxFee();
-  }
-
-  checkoutModeToggle = () => {
-    this.setState({ isCheckoutMode: !this.state.isCheckoutMode })
-  }
-
-  inputHandler = (event) => {
-    const { name, value } = event.target
-
-    this.setState({ [name]: value });
-  }
-
-  payBtnHandler = () => {
-    // 1. POST ke /transactions
-    // 2. DELETE semua cart item yg sudah dibayar
-
-    if (this.state.payment < this.renderTotalPrice()) {
-      alert(`Uang anda kurang ${this.renderTotalPrice() - this.state.payment}`)
-      return;
-    }
-
-    if (this.state.payment > this.renderTotalPrice()) {
-      alert(`Sukses anda mendapatkan kembalian ${this.state.payment - this.renderTotalPrice()}`)
-    } else if (this.state.payment === this.renderTotalPrice()) {
-      alert("Terima kasih sudah membayar dengan uang pas")
-    }
-
-    const d = new Date();
-    Axios.post(`${API_URL}/transactions`, {
-      userId: this.props.userGlobal.id,
-      address: this.state.address,
-      recipientName: this.state.recipientName,
-      totalPrice: parseInt(this.renderTotalPrice()),
-      totalPayment: parseInt(this.state.payment),
-      transactionDate: `${d.getDate()}-${d.getMonth() + 1}-${d.getFullYear()}`, // DD-MM-YYYY
-      transactionItems: this.props.cartGlobal.cartList, // Array of objects -> cart
-    })
-    .then((result) => {
-      alert("Berhasil melakukan pembayaran")
-      result.data.transactionItems.forEach((val) => {
-        this.deleteCartHandler(val.id)
+        // console.log(userEdit);
       })
-    })
-    .catch(() => {
-      alert("Terjadi kesalahan di server")
-    })
-  }
-  render() {
-    return (
-      <div className="p-5 text-center">
-        <h1>Cart</h1>
-        <div className="row mt-5">
-          <div className="col-9 text-center">
-            <table className="table">
-              <thead className="thead-light">
-                <tr>
-                  <th>Name</th>
-                  <th>Price</th>
-                  <th>Image</th>
-                  <th>Quantity</th>
-                  <th>Total Price</th>
-                  <th>Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                {this.renderCart()}
-              </tbody>
-              <tfoot className="bg-light">
-                <tr>
-                  <td colSpan="6">
-                    <button onClick={this.checkoutModeToggle} className="btn btn-success">
-                      Checkout
-                    </button>
-                  </td>
-                </tr>
-              </tfoot>
-            </table>
-          </div>
-          {
-            this.state.isCheckoutMode ? 
-            <div className="col-3">
-              {/* Form checkout */}
-              <div className="card text-left">
-                <div className="card-header">
-                  <strong>Order Summary</strong>
-                </div>
-                <div className="card-body">
-                  <div className="d-flex my-2 flex-row justify-content-between align-items-center">
-                    <span className="font-weight-bold">Subtotal Price</span>
-                    <span>Rp {this.renderSubtotalPrice()}</span>
-                  </div>
-                  <div className="d-flex my-2 flex-row justify-content-between align-items-center">
-                    <span className="font-weight-bold">Tax Fee (5%)</span>
-                    <span>Rp {this.renderTaxFee()}</span>
-                  </div>
-                  <div className="d-flex my-2 flex-row justify-content-between align-items-center">
-                    <span className="font-weight-bold">Total Price</span>
-                    <span>Rp {this.renderTotalPrice()}</span>
-                  </div>
-                </div>
-                <div className="card-body border-top">
-                  <label htmlFor="recipientName">Recipient Name</label>
-                  <input onChange={this.inputHandler} type="text" className="form-control mb-3" name="recipientName"/>
-                  <label htmlFor="address">Address</label>
-                  <input onChange={this.inputHandler} type="text" className="form-control" name="address"/>
-                </div>
-                <div className="card-footer">
-                  <div className="d-flex flex-row justify-content-between align-items-center">
-                    <input onChange={this.inputHandler} name="payment" className="form-control mx-1" type="number"/>
-                    <button onClick={this.payBtnHandler} className="btn btn-success mx-1">Pay</button>
-                  </div>
-                </div>
-              </div>
+      .catch((err) => console.log(err));
+  };
+  // const [userEdit, setUser] = useState({});
+  console.log(user)
+  useEffect(() => {
+    console.log("test")
+    user()
+  });
+
+  // FOR ADMIN CHECKING USER
+  // const get = () => {
+  //   Axios.get("http://localhost:3302/user/getuser")
+  //     .then((res) => {
+  //       console.log(res.data);
+  //     })
+  //     .catch((err) => console.log(err));
+  // };
+
+  return (
+    <>
+      <div className="wrapper">
+        <div className="reg-container">
+          <div className="UserProfile-side">
+            <div className="reg-icon">
             </div>
-            : null
-          }
+            <div className="right">
+              <h1 className="info">Your Transactions</h1>
+            </div>
+            <div className="right">
+              <form className="right-form">
+                <label>
+                  <h2 className="status">Status</h2>
+                  <p>{status}</p>
+                </label>
+                <label>
+                  <h2 className="date">Date</h2>
+                  <p>{created_at}</p>
+                </label>
+                <label>
+                  <h2 className="parcel">Order Number</h2>
+                  <p>{no_order}</p>
+                </label>
+
+                <label>
+                  <h2 className="data">Quantity</h2>
+                  <p>{quantity}</p>
+                </label>
+                <label>
+                  <h2 className="data">Parcel Price</h2>
+                  <p>{harga}</p>
+                </label>
+                <label>
+                  <h2 className="data">Total</h2>
+                  <p>{total}</p>
+                </label>
+                <Link to={`/edit/${id}`}>
+                  <Button className="reg-button" type="button">
+                  Order Detail
+                </Button>
+                 </Link>
+              </form>
+            </div>
+          </div>
+          <div className="content-side"></div>
         </div>
       </div>
-    )
-  }
+    </>
+  );
 }
 
-const mapStateToProps = state => {
-  return {
-    cartGlobal: state.cart,
-    userGlobal: state.user,
-  }
-}
-
-const mapDispatchToProps = {
-  getCartData,
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Cart);
+export default UserTransaksi;
