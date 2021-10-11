ab262858ca2e50ed3d76af53ce6b36a99c89546a
Ivanstwan <ivan.stwan@gmail.com>
2021-10-11 10:05:58 +0700

    [PWD-21/22] add uploader

--- be/helper/uploader.js
+++ be/helper/uploader.js
@@ -1,2 +1,27 @@
 const multer = require("multer");
 const fs = require("fs");
+
+module.exports = {
+  uploader: (directory, fileNamePrefix) => {
+    // Lokasi penyimpanan file
+    let defaultDirectory = "./public";
+
+    // diskStorage : untuk menyimpan file dari frontend FE ke dalam directory backend BE
+    const storage = multer.diskStorage({
+      destination: (req, res, cb) => {
+        const pathDirectory = defaultDirectory + directory;
+
+        if (fs.existsSync(pathDirectory)) {
+          console.log(
+            "Directory ada, fungsi bawah ini ngecek ada ato tidaknya directory"
+          );
+          cb(null, pathDirectory);
+        } else {
+          fs.mkdir(pathDirectory, { recursive: true }, (err) =>
+            cb(err, pathDirectory)
+          );
+        }
+      },
+    });
+  },
+};
