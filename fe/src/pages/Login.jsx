0dd44a85982d8b5ab9d3d1409165ff40020baaf6
RyanRamadhianto <86404155+RyanRamadhianto@users.noreply.github.com>
2021-10-18 16:33:24 +0700

    12

--- fe/src/pages/Login.jsx
+++ fe/src/pages/Login.jsx
@@ -78,7 +78,6 @@ class Login extends React.Component {
                     Login
                   </button>
                   <Link to={"/forgot"}>Forgot Password?</Link>
-                  <Link to={"/forgot"}>Edit Password</Link>
                 </div>
               </div>
             </div>
